14a0ec09614bd2394897a9555d31f93c36c7e1cb
Gérard Kra <150908185+gerardkra@users.noreply.github.com>
2025-12-21 11:34:39 +0100

    Delete cours/cote-azur directory

--- cours/cote-azur/M1/Semestre1/INFORMATIQUE/BASE_DE_DONNEE/TP3_SUITE.sql
+++
@@ -1,3 +0,0 @@
-SELECT preusuel
-FROM prenoms
-WHERE sexe =1 OR sexe=2 AND annais=2018 AND preusuel<>"_PRENOMS_RARES" AND dpt="06";
--- cours/cote-azur/M1/Semestre1/INFORMATIQUE/C++/exemples_codes_du_cours/somme.hh
+++
@@ -1,8 +0,0 @@
-#ifndef __SOMME_HH__
-#define __SOMME_HH__
-
-/* Déclaration de la fonction somme */
-
-double somme (double a, double b);
-
-#endif
